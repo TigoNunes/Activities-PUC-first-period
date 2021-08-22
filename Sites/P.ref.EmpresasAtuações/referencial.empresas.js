95a1fc465c7243f7b5e6a52b0e575034bb4cba96
TigoNunes <tiagonunesdecerqueira.pro@gmail.com>
2021-08-22 01:47:22 -0300

    Create referencial.empresas.js

---
+++ Sites/P.ref.EmpresasAtuações/referencial.empresas.js
@@ -0,0 +1,91 @@
+
+window.onload = () => {
+    td.onclick = () => {
+        //nacionais
+
+        let elemento = document.getElementById('nacionais1');
+        elemento.style.display = 'block';
+        
+        let elemento2 = document.getElementById('nacionais2');
+        elemento2.style.display = 'block';
+        
+        let elemento3 = document.getElementById('nacionais3');
+        elemento3.style.display = 'block';
+
+        //internacionais
+        
+        let elemento4 = document.getElementById('internacionais1');
+        elemento4.style.display = 'block';
+
+        let elemento5 = document.getElementById('internacionais2');
+        elemento5.style.display = 'block';
+
+        let elemento6 = document.getElementById('internacionais3');
+        elemento6.style.display = 'block';
+
+        let elemento7 = document.getElementById('internacionais4');
+        elemento7.style.display = 'block';
+
+        let elemento8 = document.getElementById('internacionais5');
+        elemento8.style.display = 'block';
+        
+    }
+    nac.onclick = () => {
+        //nacionais
+
+        let elemento = document.getElementById('nacionais1');
+        elemento.style.display = 'block';
+
+        let elemento2 = document.getElementById('nacionais2');
+        elemento2.style.display = 'block';
+
+        let elemento3 = document.getElementById('nacionais3');
+        elemento3.style.display = 'block';
+
+        //internacionais
+
+        let elemento4 = document.getElementById('internacionais1');
+        elemento4.style.display = 'none';
+
+        let elemento5 = document.getElementById('internacionais2');
+        elemento5.style.display = 'none';
+
+        let elemento6 = document.getElementById('internacionais3');
+        elemento6.style.display = 'none';
+
+        let elemento7 = document.getElementById('internacionais4');
+        elemento7.style.display = 'none';
+
+        let elemento8 = document.getElementById('internacionais5');
+        elemento8.style.display = 'none';
+    }
+    internac.onclick = () => {
+        //nacionais
+
+        let elemento = document.getElementById('nacionais1');
+        elemento.style.display = 'none';
+
+        let elemento2 = document.getElementById('nacionais2');
+        elemento2.style.display = 'none';
+
+        let elemento3 = document.getElementById('nacionais3');
+        elemento3.style.display = 'none';
+
+        //internacionais
+
+        let elemento4 = document.getElementById('internacionais1');
+        elemento4.style.display = 'block';
+
+        let elemento5 = document.getElementById('internacionais2');
+        elemento5.style.display = 'block';
+
+        let elemento6 = document.getElementById('internacionais3');
+        elemento6.style.display = 'block';
+
+        let elemento7 = document.getElementById('internacionais4');
+        elemento7.style.display = 'block';
+
+        let elemento8 = document.getElementById('internacionais5');
+        elemento8.style.display = 'block';
+    }
+}
